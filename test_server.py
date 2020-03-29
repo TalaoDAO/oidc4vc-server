5d9b93f11fa00a49af5b612148950d08e6a54dde
thierrythevenet1963 <thierry.thevenet@talao.io>
2020-03-29 17:25:50 +0200

    certificate responsive

--- test_server.py
+++ test_server.py
@@ -75,7 +75,7 @@ def getclaimipfs (claim_id, workspace_contract) :
 	client = ipfshttpclient.connect('/dns/ipfs.infura.io/tcp/5001/https')
 	contract=w3.eth.contract(workspace_contract,abi=constante.workspace_ABI)
 	claimdata=contract.functions.getClaim(claim_id).call()
-
+	print("claimdata = ", claimdata)
 	if claimdata[5]!="" :
 		data=client.get_json(claimdata[5])
 		return data
@@ -303,17 +303,22 @@ def GET_nameservice_reload() :
 #    affichage d'un certificat de type claim
 #############################################################
 #data='did:talao:rinkeby:ab6d2bAE5ca59E4f5f729b7275786979B17d224b:claim:b34c2a6837a9e89da5ef886d18763fb13a12615814d50a5b73ae403cb547d788'
-
+#data="did:talao:rinkeby:ab6d2bAE5ca59E4f5f729b7275786979B17d224b:claim:abf370997a7b240f56c62b8b33cc8976f9808d3889f3eed865c79e4622d90af4"
+                
 
 @app.route('/certificate/<data>', methods=['GET'])
 def show_certificate(data):
 	
+	data="did:talao:rinkeby:ab6d2bAE5ca59E4f5f729b7275786979B17d224b:claim:abf370997a7b240f56c62b8b33cc8976f9808d3889f3eed865c79e4622d90af4"
+
 	claimId=data.split(':')[5]
-	
 	workspace_contract= '0x'+data.split(':')[3]
-	
 	certificate=getclaimipfs(claimId, workspace_contract)
 	
+	if certificate == False :
+		return {"ERROR" : "No Certificate"}
+	
+
 	ok="color: rgb(251,211,5); font-size: 10px;"
 	ko="color: rgb(0,0,0);font-size: 10px;"
 	
@@ -336,7 +341,7 @@ def show_certificate(data):
 			context ["star"+str(q)+str(i)]=ko
 	
 
-	return render_template('certificate.html', **context)
+	return render_template('certificate2.html', **context)
 
 # upload des photos
 @app.route('/uploads/<filename>')
@@ -372,7 +377,7 @@ def resume() :
 	#return GETresume.getresume(truedid,mode)	
 	# appel de l API
 	print("appel de l api")
-	conn = http.client.HTTPConnection("127.0.0.1:5000")
+	conn = http.client.HTTPConnection(mode.flaskserver+':'+mode.port)
 	headers = {'Accept': 'application/json','Content-type': 'application/json'}
 	payload = { "did" : truedid}
 	data = json.dumps(payload)
