e096bef0048babf50ad64f60935f055b685f6d63
thierrythevenet1963 <thierry.thevenet@talao.io>
2020-05-31 20:38:08 +0200

    readthedoc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -35,6 +35,23 @@ extensions = [
 
 
 html_theme = "sphinx_rtd_theme"
+
+html_theme_options = {
+    'canonical_url': '',
+    'analytics_id': 'UA-XXXXXXX-1', 
+    'logo_only': False,
+    'display_version': True,
+    'prev_next_buttons_location': 'bottom',
+    'style_external_links': False,
+    'vcs_pageview_mode': '',
+    'style_nav_header_background': 'white',
+    # Toc options
+    'collapse_navigation': True,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+    'includehidden': True,
+    'titles_only': False
+}
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
