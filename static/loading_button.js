833460332bef8d2c147b306b2e894b7722fa783e
Alexandre Leclerc <jarvis@pop-os.localdomain>
2020-10-22 17:12:53 +0200

    Bug loading_button

--- static/loading_button.js
+++ static/loading_button.js
@@ -1,8 +1,15 @@
 $(document).ready(function() {
     $("#loading_button").click(function() {
  //     $(this).prop("disabled", true);
-      $(this).html(
-        `<span class="spinner-border spinner-border-sm" ></span> Loading...`
-      );
+
+      try {if (document.forms['form']['checkValidity']()) {
+        $(this).html(
+          `<span class="spinner-border spinner-border-sm" ></span> Loading...`
+        );
+      }} catch(error){
+        $(this).html(
+          `<span class="spinner-border spinner-border-sm" ></span> Loading...`
+        );
+      }
     });
 });
