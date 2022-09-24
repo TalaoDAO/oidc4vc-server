2f3e493b84fec71086f3439e3ba85f7cf9202695
Thierry Thevenet <thierry.thevenet@talao.io>
2022-09-24 11:38:42 +0200

    update site

--- routes/saas4ssi.py
+++ routes/saas4ssi.py
@@ -3,13 +3,15 @@ from device_detector import SoftwareDetector
 
 
 def init_app(app,red, mode) :
-    app.add_url_rule('/sandbox/saas4ssi',  view_func=saas_login, methods = ['GET', 'POST'])
+    app.add_url_rule('/sandbox/saas4ssi',  view_func=saas_home, methods = ['GET', 'POST'])
     app.add_url_rule('/sandbox/saas4ssi/verifier',  view_func=saas_verifier, methods = ['GET', 'POST'])
     app.add_url_rule('/sandbox/saas4ssi/device_detector',  view_func=saas_device_detector, methods = ['GET'])
     app.add_url_rule('/sandbox/saas4ssi/qrcode',  view_func=saas_qrcode, methods = ['GET'], defaults={'mode' : mode})
     app.add_url_rule('/sandbox/saas4ssi/issuer',  view_func=saas_issuer, methods = ['GET', 'POST'])
-    app.add_url_rule('/sandbox/saas4ssi/pricing',  view_func=saas_catalog, methods = ['GET', 'POST'])
+    app.add_url_rule('/sandbox/saas4ssi/pricing',  view_func=pricing, methods = ['GET', 'POST'])
     app.add_url_rule('/sandbox/saas4ssi/offers',  view_func=saas_catalog, methods = ['GET', 'POST'])
+    app.add_url_rule('/sandbox/saas4ssi/credentials',  view_func=credentials, methods = ['GET', 'POST'])
+    app.add_url_rule('/sandbox/saas4ssi/login',  view_func=saas_login, methods = ['GET', 'POST'])
 
     return
 
@@ -28,6 +30,16 @@ def saas_device_detector ():
     else :
         return jsonify('unknown device')    
 
+def saas_home():
+    return render_template("home.html")
+
+
+def credentials():
+    return render_template("credentials.html")
+
+def pricing():
+    return render_template("pricing.html")
+
 
 def saas_catalog():
     if request.method == "GET" :
@@ -35,7 +47,7 @@ def saas_catalog():
 
 def saas_login():
     if request.method == "GET" :
-        return render_template("saas4ssi.html")
+        return render_template("saas_login.html")
     else :
         if request.form["login_name"].lower() not in ['ebsilux', "admin1234", "guest", "arago1234"] :
             return redirect('/sandbox/saas4ssi')
