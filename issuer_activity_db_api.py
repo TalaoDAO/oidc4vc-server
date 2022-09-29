81b9b65a1b967060905c6c0fbc89587257ffdb20
Thierry Thevenet <thierry.thevenet@talao.io>
2022-09-29 14:46:05 +0200

    bug fix

---
+++ issuer_activity_db_api.py
@@ -0,0 +1,37 @@
+import json
+import traceback
+import logging
+import sys
+import sqlite3
+logging.basicConfig(level=logging.INFO)
+
+def create(client_id, record) :
+    if not isinstance(record, str) :
+        record = json.dumps(record)
+    conn = sqlite3.connect("issuer_activity.db")
+    c = conn.cursor()
+    db_data = { "client_id" : client_id ,"data" : record}
+    try :
+        c.execute("INSERT INTO activity VALUES (:client_id, :data)", db_data)
+        conn.commit()
+    except sqlite3.Error as er:
+        print('SQLite error: %s' % (' '.join(er.args)))
+        print("Exception class is: ", er.__class__)
+        print('SQLite traceback: ')
+        exc_type, exc_value, exc_tb = sys.exc_info()
+        print(traceback.format_exception(exc_type, exc_value, exc_tb))
+        logging.error('DB error')
+        return None
+    conn.close()
+    return True
+
+
+def list(client_id) :
+    """ Return list of username """
+    conn = sqlite3.connect("issuer_activity.db")
+    data = {"client_id" : client_id}
+    c = conn.cursor()
+    c.execute("SELECT data FROM activity WHERE client_id = :client_id ", data)
+    db_select = [x[0]for x in c.fetchall()]
+    conn.close()
+    return db_select
