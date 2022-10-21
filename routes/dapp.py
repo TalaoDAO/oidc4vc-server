444916b7093a8dbc8bb939765ab445103bbffbbf
Thierry Thevenet <thierry.thevenet@talao.io>
2022-10-21 08:16:22 +0200

    update

--- routes/dapp.py
+++ routes/dapp.py
@@ -1,5 +1,6 @@
 from flask import render_template
 
+# https://github.com/airgap-it/beacon-sdk
 
 def init_app(app,red, mode) :
     app.add_url_rule('/sandbox/saas4ssi/dapp',  view_func=dapp_wallet, methods = ['GET', 'POST'])
