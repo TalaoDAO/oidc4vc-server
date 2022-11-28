91d976a448c13ddeb2c1582579464050534ac2dd
Thierry Thevenet <thierry.thevenet@talao.io>
2022-11-28 18:47:59 +0100

    updatre gevent

--- gunicornconf.py
+++ gunicornconf.py
@@ -3,13 +3,13 @@
 workers = 5
 loglevel = 'info'
 
-worker_class = 'gevent' 
+#worker_class = 'gevent' 
 
 errorlog = "-"
 accesslog = "-"
 
-timeout = 3 * 60  # 3 minutes
-keepalive = 5 * 24 * 60 * 60  # 5 days
+timeout = 30  # sec
+keepalive = 504  # sec
 capture_output = True
 
 
