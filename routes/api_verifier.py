eecc3109fc2cedaedd5052140df8c84109021339
Thierry Thevenet <thierry.thevenet@talao.io>
2023-07-03 08:55:59 +0200

    bug

--- routes/api_verifier.py
+++ routes/api_verifier.py
@@ -486,11 +486,12 @@ def login_qrcode(red, mode):
     if qrcode_page == 'altme_connect.html' and request.MOBILE:
         qrcode_page = 'altme_connect_mobile.html' 
 
+    jpma_text = "Preuve de + de 15 ans"
     # For jpma only
     if verifier_data['vc'] == "AgeOver18" :
         jpma_text = "Preuve de + de 18 ans"
-    if verifier_data['vc'] == "AgeOver15" :
-        jpma_text = "Preuve de + de 15 ans"
+    #if verifier_data['vc'] == "AgeOver15" :
+    #    jpma_text = "Preuve de + de 15 ans"
     
     return render_template(qrcode_page,
                             back_button = False,
